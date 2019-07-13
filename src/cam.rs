f80daf9c685cefc758705f8c2ea5e7920e304e37
Dhs92 <forehand.alex@gmail.com>
2019-07-13 14:02:58 -0700

    Added handling for values out of range

--- src/cam.rs
+++ src/cam.rs
@@ -11,7 +11,18 @@ pub struct Kraken<'a> {
     iface: u8,
 }
 
+/// # Example
+///
+/// ```
+/// use crate::camctl::Kraken;
+///
+/// let ctx = libusb::Context::new()?;
+/// let kraken = Kraken::from(ctx)?;
+///
+/// kraken.set_fan(75); // sets the fan speed to 75%
+/// ```
 impl<'a> Kraken<'a> {
+    /// Constructs a new `Kraken` from [libusb::Context](https://docs.rs/libusb/0.3.0/libusb/struct.Context.html)
     pub fn from(ctx: &'a libusb::Context) -> libusb::Result<Self> {
         let device = Kraken {
             iface: 0, // magic number
@@ -35,24 +46,58 @@ impl<'a> Kraken<'a> {
         Err(libusb::Error::NotFound)
     }
 
+    /// Set the fan speed by percent, `speed` should be 0-100
+    ///
+    /// If the speed is over 100, the method will return without side-effects
     pub fn set_fan(&self, speed: u8) -> libusb::Result<()> {
-        let mut interface = self.device.open()?;
+        match speed {
+            0..=100 => {
+                let mut interface = self.device.open()?;
 
-        if interface.kernel_driver_active(self.iface)? {
-            interface.detach_kernel_driver(self.iface)?;
-        }
+                if interface.kernel_driver_active(self.iface)? {
+                    interface.detach_kernel_driver(self.iface)?;
+                }
 
-        interface.claim_interface(self.iface)?;
+                interface.claim_interface(self.iface)?;
 
-        // magic payload, https://github.com/leaty/camctl/blob/master/camctl#L31
-        let payload: [u8; 24] = [
-            2, 77, 0, 0, speed, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ];
+                // magic payload, https://github.com/leaty/camctl/blob/master/camctl#L31
+                let payload: [u8; 24] = [
+                    2, 77, 0, 0, speed, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                ];
 
-        // write the payload, 10 second timeout
-        match interface.write_bulk(1, &payload, Duration::new(10, 0)) {
-            Err(libusb::Error::Io) => {
-                log::warn!("Write failed!");
+                // write the payload, 10 second timeout
+                match interface.write_bulk(1, &payload, Duration::new(10, 0)) {
+                    Err(libusb::Error::Io) => {
+                        log::warn!("Write failed!");
+                        match interface.release_interface(self.iface) {
+                            Err(libusb::Error::Success) | Ok(()) => {
+                                log::info!("Interface released successfully")
+                            }
+                            Err(e) => log::warn!("Unable to release interface: {}", e),
+                        }
+                    }
+
+                    Err(_) => {
+                        log::error!("An unknown error has occurred");
+                        match interface.release_interface(self.iface) {
+                            Err(libusb::Error::Success) | Ok(()) => {
+                                log::info!("Interface released successfully")
+                            }
+                            Err(e) => log::warn!("Unable to release interface: {}", e),
+                        }
+                    }
+                    Ok(_) => (),
+                };
+
+                if !interface.kernel_driver_active(self.iface)? {
+                    match interface.attach_kernel_driver(self.iface) {
+                        Err(libusb::Error::Success) | Ok(()) => {
+                            log::info!("Kernel driver re-attached successfully")
+                        }
+                        Err(e) => log::warn!("Kernel driver could not be re-attached: {}", e),
+                    } // re-attach the kernel driver
+                }
+
                 match interface.release_interface(self.iface) {
                     Err(libusb::Error::Success) | Ok(()) => {
                         log::info!("Interface released successfully")
@@ -60,53 +105,62 @@ impl<'a> Kraken<'a> {
                     Err(e) => log::warn!("Unable to release interface: {}", e),
                 }
             }
-
-            Err(_) => {
-                log::error!("An unknown error has occurred");
-                match interface.release_interface(self.iface) {
-                    Err(libusb::Error::Success) | Ok(()) => {
-                        log::info!("Interface released successfully")
-                    }
-                    Err(e) => log::warn!("Unable to release interface: {}", e),
-                }
-            }
-            Ok(_) => (),
-        };
-
-        if !interface.kernel_driver_active(self.iface)? {
-            match interface.attach_kernel_driver(self.iface) {
-                Err(libusb::Error::Success) | Ok(()) => {
-                    log::info!("Kernel driver re-attached successfully")
-                }
-                Err(e) => log::warn!("Kernel driver could not be re-attached: {}", e),
-            } // re-attach the kernel driver
+            _ => log::warn!("Speed provided was: {}", speed),
         }
-
-        match interface.release_interface(self.iface) {
-            Err(libusb::Error::Success) | Ok(()) => log::info!("Interface released successfully"),
-            Err(e) => log::warn!("Unable to release interface: {}", e),
-        }
-
         Ok(())
     }
 
+    /// Set the pump speed by percent, `speed` should be 0-100
+    ///
+    /// If the speed is over 100, the method will return without side-effects
     pub fn set_pump(&self, speed: u8) -> libusb::Result<()> {
-        let mut interface = self.device.open()?;
+        match speed {
+            0..=100 => {
+                let mut interface = self.device.open()?;
 
-        if interface.kernel_driver_active(self.iface)? {
-            interface.detach_kernel_driver(self.iface)?;
-        }
+                if interface.kernel_driver_active(self.iface)? {
+                    interface.detach_kernel_driver(self.iface)?;
+                }
 
-        interface.claim_interface(self.iface)?;
+                interface.claim_interface(self.iface)?;
 
-        // magic payload, https://github.com/leaty/camctl/blob/master/camctl#L31
-        let payload: [u8; 24] = [
-            2, 77, 64, 0, speed, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ];
+                // magic payload, https://github.com/leaty/camctl/blob/master/camctl#L31
+                let payload: [u8; 24] = [
+                    2, 77, 64, 0, speed, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                ];
 
-        match interface.write_bulk(1, &payload, Duration::new(10, 0)) {
-            Err(libusb::Error::Io) => {
-                log::warn!("Write failed!");
+                match interface.write_bulk(1, &payload, Duration::new(10, 0)) {
+                    Err(libusb::Error::Io) => {
+                        log::warn!("Write failed!");
+                        match interface.release_interface(self.iface) {
+                            Err(libusb::Error::Success) | Ok(()) => {
+                                log::info!("Interface released successfully")
+                            }
+                            Err(e) => log::warn!("Unable to release interface: {}", e),
+                        }
+                    }
+
+                    Err(_) => {
+                        log::error!("An unknown error has occurred");
+                        match interface.release_interface(self.iface) {
+                            Err(libusb::Error::Success) | Ok(()) => {
+                                log::info!("Interface released successfully")
+                            }
+                            Err(e) => log::warn!("Unable to release interface: {}", e),
+                        }
+                    }
+                    Ok(_) => (),
+                };
+
+                if !interface.kernel_driver_active(self.iface)? {
+                    match interface.attach_kernel_driver(self.iface) {
+                        Err(libusb::Error::Success) | Ok(()) => {
+                            log::info!("Kernel driver re-attached successfully")
+                        }
+                        Err(e) => log::warn!("Kernel driver could not be re-attached: {}", e),
+                    } // re-attach the kernel driver
+                }
+
                 match interface.release_interface(self.iface) {
                     Err(libusb::Error::Success) | Ok(()) => {
                         log::info!("Interface released successfully")
@@ -114,31 +168,7 @@ impl<'a> Kraken<'a> {
                     Err(e) => log::warn!("Unable to release interface: {}", e),
                 }
             }
-
-            Err(_) => {
-                log::error!("An unknown error has occurred");
-                match interface.release_interface(self.iface) {
-                    Err(libusb::Error::Success) | Ok(()) => {
-                        log::info!("Interface released successfully")
-                    }
-                    Err(e) => log::warn!("Unable to release interface: {}", e),
-                }
-            }
-            Ok(_) => (),
-        };
-
-        if !interface.kernel_driver_active(self.iface)? {
-            match interface.attach_kernel_driver(self.iface) {
-                Err(libusb::Error::Success) | Ok(()) => {
-                    log::info!("Kernel driver re-attached successfully")
-                }
-                Err(e) => log::warn!("Kernel driver could not be re-attached: {}", e),
-            } // re-attach the kernel driver
-        }
-
-        match interface.release_interface(self.iface) {
-            Err(libusb::Error::Success) | Ok(()) => log::info!("Interface released successfully"),
-            Err(e) => log::warn!("Unable to release interface: {}", e),
+            _ => log::warn!("Speed provided was: {}", speed),
         }
 
         Ok(())
