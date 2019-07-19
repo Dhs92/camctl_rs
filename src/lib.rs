a1151fd9842d1b1eeea5080b7a6c50a51c1d7236
Dhs92 <forehand.alex@gmail.com>
2019-07-19 10:54:59 -0700

    Moved example from cam.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+//! # Example
+//!
+//! ```
+//! use camctl_rs::Kraken;
+//!
+//! let ctx = libusb::Context::new().unwrap();
+//! let kraken = Kraken::from(&ctx).unwrap();
+//!
+//! kraken.set_fan(75); // sets the fan speed to 75%
+//! ```
 mod cam;
 pub use cam::*;
